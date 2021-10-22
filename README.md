32304234ff828504da44e0bb358af526a24ff882
RahulSoni0 <rahul.kumar.soni2511@gmail.com>
2021-10-22 17:49:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Please give a Read
+
+To the Contributors - Don't Fork this repo, Directly clone it through Android Studio and keep working and updating the repo.
+Please try to add comments for each part or feature so that teammates can easily understand.
+This Repo will become public as soon as the event starts.
+Also it is requested to change your settings and push your file directly on "master" branch.
+
+
+Thank You.
---
+++ app/src/main/java/com/devcomm/hackathon/MainActivity.java
@@ -0,0 +1,17 @@
+package com.devcomm.hackathon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import devcomm.hackathon.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //bhai bekar ka commit
+    }
+}
